107641e43ce3fdcde902cdcfc547af02e9aaf8d2
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-15 10:05:04 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+// Assignability Error
+    //Example:
+let message:string = "Hello World";
+console.log(message);
+message = 123; //Error
+
+    //Example:
+let message = "Hello World";
+message = 6; // Error
+console.log(message);
+  
+
